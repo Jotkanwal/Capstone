a6d0c61995bb48bad70da1baefb02056e34f1078
Ben Vokey <31905501+Vioarr@users.noreply.github.com>
2020-10-21 19:50:43 -0400

    Cleaned up script formatting

--- DS18B20_TempTestScript.py
+++ DS18B20_TempTestScript.py
@@ -5,17 +5,17 @@ from datetime import datetime #used for creating timestamps
 DS18B20_SENSOR = W1ThermSensor()
 
 while True:   
-	#defining current time and getting the temps from the DS18B20
+    #defining current time and getting the temps from the DS18B20
     now = datetime.now().strftime("%H:%M:%S")
     temps = DS18B20_SENSOR.get_temperatures([
-    W1ThermSensor.DEGREES_C,
-    W1ThermSensor.DEGREES_F,
-    W1ThermSensor.KELVIN]);
+    	W1ThermSensor.DEGREES_C,
+    	W1ThermSensor.DEGREES_F,
+    	W1ThermSensor.KELVIN]);
 	
-	#printing a formatted ssh entry
+    #printing a formatted ssh entry
     print("------------", now, "------------");
     print("The temperature is %s C, %s F, %s K"
           % (temps[0], temps[1], temps[2]));
 	
-	#wait two seconds
+    #wait two seconds
     time.sleep(2);
